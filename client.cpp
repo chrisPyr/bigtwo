ab9f7afa74a5a719e10b5db3e5fd1ba85751c8e5
gcli <star106121@gmail.com>
2021-02-04 17:48:56 +0800

    [Style] Format the source code

--- client.cpp
+++ client.cpp
@@ -1,32 +1,30 @@
-#include <iostream>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
+#include <iostream>
+#include <netinet/in.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 int sockfd = 0;
-void* recvdata(void*){
-  while(1){
+void *recvdata(void *) {
+  while (1) {
     char recvmsg[255] = {};
-    if(recv(sockfd, recvmsg, sizeof(recvmsg), 0) <= 0) pthread_exit(NULL);
-    cout<< recvmsg << "9999\n";
-
+    if (recv(sockfd, recvmsg, sizeof(recvmsg), 0) <= 0)
+      pthread_exit(NULL);
+    cout << recvmsg << "9999\n";
   }
-
-
 }
 
-int main(int arc, char *argv[]){
+int main(int arc, char *argv[]) {
   pthread_t t1;
-  //int sockfd = 0;
+  // int sockfd = 0;
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  if(sockfd == -1){
+  if (sockfd == -1) {
     cout << "Fail to create a socket\n";
   }
 
@@ -37,24 +35,25 @@ int main(int arc, char *argv[]){
   info.sin_addr.s_addr = inet_addr("127.0.0.1");
   info.sin_port = htons(55146);
 
-  int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
-  if(err == -1){
-    cout<< "Connection error\n";
+  int err = connect(sockfd, (struct sockaddr *)&info, sizeof(info));
+  if (err == -1) {
+    cout << "Connection error\n";
   }
 
-  pthread_create(&t1,NULL,recvdata,NULL);
+  pthread_create(&t1, NULL, recvdata, NULL);
 
   char message[255] = {"Hi"};
   char recvmessage[100] = {};
-  while(message != "end"){
-    //char recvmsg[255] = {};
+  while (message != "end") {
+    // char recvmsg[255] = {};
     cout << "fff\n";
-    if(send(sockfd, message, sizeof(message), 0) < 0) break;
-    //recv(sockfd, recvmsg, sizeof(recvmsg), 0);
+    if (send(sockfd, message, sizeof(message), 0) < 0)
+      break;
+    // recv(sockfd, recvmsg, sizeof(recvmsg), 0);
 
-    //cout << recvmsg << "\n";
-    memset(message,0,sizeof(message));
-    cin>>message;
+    // cout << recvmsg << "\n";
+    memset(message, 0, sizeof(message));
+    cin >> message;
   }
   cout << "close Socket\n";
   close(sockfd);
--- server.cpp
+++ server.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <pthread.h>
 
 using namespace std;
 int forClientSockfd = 0;
 
-void* recvdata(void*){
-  while(1){
+void *recvdata(void *) {
+  while (1) {
     char inputBuffer[255] = {};
-    recv(forClientSockfd, inputBuffer, sizeof(inputBuffer),0);
-    cout<< inputBuffer << "\n";
+    recv(forClientSockfd, inputBuffer, sizeof(inputBuffer), 0);
+    cout << inputBuffer << "\n";
   }
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
   pthread_t t1;
   char message[255] = {"Hi, I'm server."};
-  int sockfd = 0;//, forClientSockfd =0;
-  sockfd = socket(AF_INET,SOCK_STREAM,0);
+  int sockfd = 0; //, forClientSockfd =0;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(sockfd == -1){
-      cout<< "Fail to create a socket\n";
-    }
-  int flag =1;
-  struct sockaddr_in serverInfo,clientInfo;
+  if (sockfd == -1) {
+    cout << "Fail to create a socket\n";
+  }
+  int flag = 1;
+  struct sockaddr_in serverInfo, clientInfo;
   unsigned int addrlen = sizeof(clientInfo);
   memset(&clientInfo, 0, sizeof(serverInfo));
 
@@ -34,19 +34,19 @@ int main(int argc, char *argv[]){
   serverInfo.sin_addr.s_addr = INADDR_ANY;
   serverInfo.sin_port = htons(55146);
   setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(int));
-  bind(sockfd,(struct sockaddr *)&serverInfo, sizeof(serverInfo));
-  listen(sockfd,5);
-  forClientSockfd = accept(sockfd,(struct sockaddr *)&clientInfo,&addrlen);
+  bind(sockfd, (struct sockaddr *)&serverInfo, sizeof(serverInfo));
+  listen(sockfd, 5);
+  forClientSockfd = accept(sockfd, (struct sockaddr *)&clientInfo, &addrlen);
 
-  pthread_create(&t1,NULL,recvdata,NULL);
+  pthread_create(&t1, NULL, recvdata, NULL);
 
-  while(1){
+  while (1) {
     char inputBuffer[255] = {};
-    send(forClientSockfd,message,sizeof(message),0);
-    //recv(forClientSockfd,inputBuffer,sizeof(inputBuffer),0);
-    //cout<< inputBuffer <<"\n";
-    memset(message,0,sizeof(message));
-    cin>>message;
+    send(forClientSockfd, message, sizeof(message), 0);
+    // recv(forClientSockfd,inputBuffer,sizeof(inputBuffer),0);
+    // cout<< inputBuffer <<"\n";
+    memset(message, 0, sizeof(message));
+    cin >> message;
   }
   return 0;
 }
